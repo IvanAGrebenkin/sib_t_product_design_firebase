c617c987abbc399fa9b2c7c888788aa33d0bf503
tex1 <ivanagrebenkin@gmail.com>
2022-03-29 16:39:53 +0700

    Предаварително оформлена страница авторизации.

--- lib/Screens/auth_screen.dart
+++ lib/Screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Utils/decoration.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -10,28 +11,71 @@ class AuthScreen extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 50),// Отступ
-              Container(),//Логотип "Сибирские товары"
-              SizedBox(height: 50),// Отступ
-              Text('Логин:'),
-              SizedBox(height: 50),// Отступ
-              TextField(),// Поле ввода Логина
-              SizedBox(height: 50),// Отступ
-              Text('Пароль:'),
-              SizedBox(height: 50),// Отступ
-              TextField(),// Поле ввода Пароля
-              SizedBox(height: 50),// Отступ
-              ElevatedButton(
-                onPressed: (){},
-                child: Text('Войти'),),// Кнопка "Войти"
-              SizedBox(height: 50),// Отступ
-              InkWell(
-                child: Text('Регистрация'),
-              ),// Ссылка на "Регистрацию"
-              SizedBox(height: 50),// Отступ
-              InkWell(
-                child: Text('Забыли пароль?'),
-              ),// Ссылка на "Забыли пароль?"
+              // Container(
+              //   // height:100,
+              //   height: double.infinity,
+              // ),
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.bottomCenter,
+                    image: AssetImage('assets/images/sibt_auth_ background.png'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                // width: double.infinity,
+                // height: double.infinity,
+                child: Center(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 100),// Отступ
+                      SizedBox(
+                        width: 165,
+                        height: 105,
+                        child: Image.asset ('assets/images/logo SibTov.png')),//Логотип "Сибирские товары"
+                      const SizedBox(height: 35),// Отступ
+                      SizedBox(width: 244, height: 40,
+                        child: TextFormField(
+                            decoration: const InputDecoration(
+                              enabledBorder: textFormFieldDecoration,
+                              focusedBorder: textFormFieldDecoration,
+                              filled: true,
+                              fillColor: Color(0xffECEFF1),
+                              labelText: 'Логин',),
+
+                        ),
+                      ),// Поле ввода Логина
+                      SizedBox(height: 40),// Отступ
+                      SizedBox(width: 244, height: 40,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            enabledBorder: textFormFieldDecoration,
+                            focusedBorder: textFormFieldDecoration,
+                            filled: true,
+                            fillColor: Color(0xffECEFF1),
+                            labelText: 'Пароль',),
+
+                        ),
+                      ),// Поле ввода Пароля
+                      SizedBox(height: 40),// Отступ
+                      ElevatedButton(
+                        onPressed: (){},
+                        child: Text('Войти'),),// Кнопка "Войти"
+                      SizedBox(height: 380),// Отступ
+                      // InkWell(
+                      //   child: Text('Регистрация'),
+                      // ),// Ссылка на "Регистрацию"
+                      // SizedBox(height: 50),// Отступ
+                      // InkWell(
+                      //   child: Text('Забыли пароль?'),
+                      // ),// Ссылка на "Забыли пароль?"
+                      // SizedBox(height: 50),// Отступ
+
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
---
+++ lib/Utils/decoration.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const textFormFieldDecoration = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(36.0)),
+  borderSide: BorderSide(
+    color: Color(0xffECEFF1),
+    width: 2.0,
+  ),
+);
