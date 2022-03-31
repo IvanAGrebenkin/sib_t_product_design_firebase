d65bada42f774c403d4de27286f4bccf17142e68
tex1 <ivanagrebenkin@gmail.com>
2022-03-31 10:50:52 +0700

    Изменена верси kotlin для устранения ошибок при компиляции apk файла (запустил Tools/Kotlin/Configure Kotlin in Project).

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   runApp(const SibTProductDesignApp());
   // runApp(const AuthScreen());
   // runApp(const ConsentToUse());
+  //
 }
 
 class SibTProductDesignApp extends StatefulWidget {
