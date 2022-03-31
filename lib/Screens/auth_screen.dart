ca0194221ad294a4e9af4a33da6d3f74a113ba14
Admin <ivanagrebenkin@gmail.com>
2022-03-31 22:28:55 +0700

    Реализована адаптация макета экрана авторизации под разрешение экрана устройства.

--- lib/Screens/auth_screen.dart
+++ lib/Screens/auth_screen.dart
@@ -9,67 +9,62 @@ class AuthScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    alignment: Alignment.bottomCenter,
-                    image: AssetImage('assets/images/sibt_auth_background.png'),
-                    fit: BoxFit.fitWidth,
-                  ),// Задний фон
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Center(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 100),// Отступ
-                      SizedBox(
-                        width: 165,
-                        height: 105,
-                        child: Image.asset ('assets/images/logo SibTov.png')),//Логотип "Сибирские товары"
-                      const SizedBox(height: 35),// Отступ
-                      SizedBox(width: 244, height: 40,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            enabledBorder: textFormFieldDecoration,
-                            focusedBorder: textFormFieldDecoration,
-                            filled: true,
-                            fillColor: Color(0xffECEFF1),
-                            labelText: 'Логин',),
+          // physics: const NeverScrollableScrollPhysics(),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const SizedBox(height: 100),// Отступ
+                  SizedBox(
+                      width: 165,
+                      height: 105,
+                      child: Image.asset ('assets/images/logo SibTov.png')),//Логотип "Сибирские товары"
+                  const SizedBox(height: 35),// Отступ
+                  SizedBox(width: 244, height: 40,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        enabledBorder: textFormFieldDecoration,
+                        focusedBorder: textFormFieldDecoration,
+                        filled: true,
+                        fillColor: Color(0xffECEFF1),
+                        labelText: 'Логин',),
+                    ),
+                  ),// Поле ввода Логина
+                  const SizedBox(height: 40),// Отступ
+                  SizedBox(width: 244, height: 40,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        enabledBorder: textFormFieldDecoration,
+                        focusedBorder: textFormFieldDecoration,
+                        filled: true,
+                        fillColor: Color(0xffECEFF1),
+                        labelText: 'Пароль',),
+                    ),
+                  ),// Поле ввода Пароля
+                  const SizedBox(height: 40),// Отступ
+                  SizedBox(
+                    width:154,
+                    height:42,
+                    child: ElevatedButton(
+                      style: elevatedButtonDecoration,
+                      onPressed: (){Navigator.pushNamed(context, '/consent_to_use');},
+                      child: const Text('Войти',
+                        style: TextStyle(
+                          fontSize: 18,
                         ),
-                      ),// Поле ввода Логина
-                      const SizedBox(height: 40),// Отступ
-                      SizedBox(width: 244, height: 40,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            enabledBorder: textFormFieldDecoration,
-                            focusedBorder: textFormFieldDecoration,
-                            filled: true,
-                            fillColor: Color(0xffECEFF1),
-                            labelText: 'Пароль',),
-                        ),
-                      ),// Поле ввода Пароля
-                      const SizedBox(height: 40),// Отступ
-                      SizedBox(
-                        width:154,
-                        height:42,
-                        child: ElevatedButton(
-                            style: elevatedButtonDecoration,
-                            onPressed: (){Navigator.pushNamed(context, '/consent_to_use');},
-                            child: const Text('Войти',
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                            ),
-                        ),
-                      ),// Кнопка 'Войти'
-                      const SizedBox(height: 380),// Отступ
-                    ],
+                      ),
+                    ),
                   ),
-                ),
+                  const Expanded(child: SizedBox()),// Отступ
+                  SizedBox(child: Image.asset ('assets/images/sibt_auth_background.png')),// фоновый рисунок
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
