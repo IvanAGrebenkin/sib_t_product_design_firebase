3bb911aec1236039e0ce3ed068115d5817830521
Admin <ivanagrebenkin@gmail.com>
2022-04-01 21:37:56 +0700

    Реализована логика работы списков на экране выбора кастрюли.

--- lib/Screens/PanScreens/pan_selection_screen.dart
+++ lib/Screens/PanScreens/pan_selection_screen.dart
@@ -14,11 +14,14 @@ class _PanSelectionState extends State<PanSelection> {
   get pageName => 'Кастрюли';
 
   // Переменне
-  bool _isDisable=true;// Переменная активности второго списка
-  bool isVisible = false;// Переменная видимости кнопки
-  List <String> namesList = <String>['...'];// Переменная для определения необходимого списка с наименованиями изделий
+  bool _isPanShapeDisable=true;// Переменная активности списка формы изделия
+  bool _isPanSizeDisable=true;// Переменная активности списка формы изделия
+  bool _isButtonVisible = false;// Переменная видимости кнопки
+  List <String> panShapeList = <String>['...'];// Переменная для определения необходимого списка формы корпуса
+  List <String> panSizeList = <String>['...'];// Переменная для определения необходимого списка вместимости изделия
   String panTypeValue = '...';// Переменная для хранения выбранного значения в списке группы изделий
-  String panShapeValue = '...';// Переменная для хранения выбранного значения в списке наименования изделия
+  String panShapeValue = '...';// Переменная для хранения выбранного значения в списке формы корпуса
+  String panSizeValue = '...';// Переменная для хранения выбранного значения в списке вместимости изделия
 
   @override
   Widget build(BuildContext context) {
@@ -29,86 +32,161 @@ class _PanSelectionState extends State<PanSelection> {
         body: Container(
           decoration: backgroundOfOthersScreen,// Задание фонового изображения
           child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 50,),
-                  const Text('Выберите тип кастрюли:'),
-                  DropdownButton(
-                    value: panTypeValue,
-                    items: listOfPanTypes.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: MediaQuery.of(context).size.width,
+                minHeight: MediaQuery.of(context).size.height,
+              ),// Ограничения в соответствии с разрешением экрана устройства
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const Expanded(child: SizedBox(height: 50,)),// Отступ по вертикали
+                    const Text('Выберите тип кастрюли:'),
+                    DropdownButton(
+                      value: panTypeValue,
+                      items: listOfPanTypes.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );}).toList(),
+                      onChanged: (String? newValue){ setState(() {
+                        panTypeValue = newValue!;
+                        if (newValue == '...') {
+                          panShapeValue = '...';
+                          panSizeValue = '...';
+                          _isPanShapeDisable = true;
+                          _isPanSizeDisable= true;
+                          _isButtonVisible=false;
+                        }
+                        else if (newValue == 'Кастрюля с ободком'){
+                          panShapeList=typesOfPanWithRimShapesList;
+                          panShapeValue = '...';
+                          panSizeValue = '...';
+                          _isPanShapeDisable = false;
+                          _isPanSizeDisable= true;
+                          _isButtonVisible=false;}
+                        else if (newValue == 'Кастрюля без ободка'){
+                          panShapeList=typesOfPanWithoutRimShapesList;
+                          panShapeValue = '...';
+                          panSizeValue = '...';
+                          _isPanShapeDisable = false;
+                          _isPanSizeDisable= true;
+                          _isButtonVisible=false;}
+                      });},
+                    ),// Список выбора типа кастрюли
+                    const SizedBox(height: 50,),
+                    const Text('Выберите форму корпуса:'),
+                    DropdownButton(
+                      value: panShapeValue,
+                      items: panShapeList.map<DropdownMenuItem<String>>((String value) {return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
                       );}).toList(),
-                    onChanged: (String? newValue){ setState(() {
-                      panTypeValue = newValue!;
-                      if (newValue == '...') {panShapeValue = '...'; _isDisable = true; isVisible=false;}
-                      else if (newValue == 'Кастрюля с ободком'){namesList=typesOfPanWithRimShapesList;panShapeValue = '...'; _isDisable = false;isVisible=false;}
-                      else if (newValue == 'Кастрюля без ободка'){namesList=typesOfPanWithoutRimShapesList;panShapeValue = '...'; _isDisable = false;isVisible=false;}
-                    });},
-                  ),
-                  const SizedBox(height: 50,),
-                  const Text('Выберите форму корпуса:'),
-                  DropdownButton(
-                    value: panShapeValue,
-                    items: namesList.map<DropdownMenuItem<String>>((String value) {return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );}).toList(),
-                    onChanged: _isDisable? null : (String? newValue1){
-                      setState(() {
-                        panShapeValue = newValue1!;
-                        if (panShapeValue == '...') {isVisible = false;}
-                        else {isVisible = true;}
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 50,),
-                  Text('Группа изделий: "$panTypeValue",',
-                    textAlign: TextAlign.start,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    child: Text('Наименование изделия: "$panShapeValue"',
-                      textAlign: TextAlign.center,),
-                  ),
-                  const SizedBox(height: 50,),
-                  Visibility(
-                    visible: isVisible,
-                    child: SizedBox(width:250, height:55,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            PassArgumentsScreen.routeName,
-                            arguments: PassedArguments(
-                              panTypeValue,
-                              panShapeValue,
+                      onChanged: _isPanShapeDisable? null : (String? newValue1){
+                        setState(() {
+                          panShapeValue = newValue1!;
+                          if (panShapeValue == '...') {
+                            panSizeValue = '...';
+                            _isPanSizeDisable=true;
+                            _isButtonVisible = false;
+                          }
+                          else if (panTypeValue == 'Кастрюля с ободком' && panShapeValue == 'Цилиндрическая'){
+                            panSizeList=panCylindricalWithRimSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                          else if (panTypeValue == 'Кастрюля с ободком' && panShapeValue == 'Сферическая'){
+                            panSizeList=panSphericalWithRimSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                          else if (panTypeValue == 'Кастрюля без ободка' && panShapeValue == 'Цилиндрическая'){
+                            panSizeList=panCylindricalWithoutRimSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                          else if (panTypeValue == 'Кастрюля без ободка' && panShapeValue == 'Сферическая'){
+                            panSizeList=panSphericalWithoutRimSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                          else if (panTypeValue == 'Кастрюля без ободка' && panShapeValue == 'Грушевидная'){
+                            panSizeList=panPearShapedWithoutRimSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                          else if (panTypeValue == 'Кастрюля без ободка' && panShapeValue == 'Позница'){
+                            panSizeList=poznicaSizeList;
+                            panSizeValue = '...';
+                            _isPanSizeDisable = false;
+                            _isButtonVisible=false;}
+                        });
+                      },
+                    ),// Список выбора формы корпуса
+                    const SizedBox(height: 50,),
+                    const Text('Выберите вместимость:'),
+                    DropdownButton(
+                      value: panSizeValue,
+                      items: panSizeList.map<DropdownMenuItem<String>>((String value) {return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );}).toList(),
+                      onChanged: _isPanSizeDisable? null : (String? newValue2){
+                        setState(() {
+                          panSizeValue = newValue2!;
+                          if (panSizeValue == '...') {_isButtonVisible = false;}
+                          else {_isButtonVisible=true;}
+                        });
+                      },
+                    ),// Список выбора вместимости изделия
+                    const SizedBox(height: 50,),
+                    Text('Выбран тип кастрюли: "$panTypeValue",',
+                      textAlign: TextAlign.start,
+                    ),// Вывод выбранного типа кастрюли
+                    Text('Выбрана форма: "$panShapeValue"',
+                      textAlign: TextAlign.center,),// Вывод выбранной формы изделия
+                    Text('Выбрана вместимость: "$panSizeValue"',
+                      textAlign: TextAlign.center,),// Вывод выбранной вместимости изделия
+                    const SizedBox(height: 50,),
+                    Visibility(
+                      visible: _isButtonVisible,
+                      child: SizedBox(width:250, height:55,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                              context,
+                              PassArgumentsScreen.routeName,
+                              arguments: PassedArguments(
+                                panTypeValue,
+                                panShapeValue,
+                              ),
+                            );
+                          },
+                          child: const Text('Выбрать чертеж',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              inherit: false,
+                              fontSize:16,
+                              fontWeight: FontWeight.w600,
                             ),
-                          );
-                        },
-                        child: const Text('Появляющаяся кнопка',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            inherit: false,
-                            fontSize:16,
-                            fontWeight: FontWeight.w600,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(10),
+                            // primary: Colors.lightGreen,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
                           ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(10),
-                          // primary: Colors.lightGreen,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0)),
-                        ),
                       ),
-                    ),
-                  ),// Появляющаяся кнопка
-                ],
+                    ),// Появляющаяся кнопка
+                    // const Expanded(child: SizedBox()),
+                    const Expanded(child: SizedBox(height: 50,)),// Отступ по вертикали
+                  ],
+                ),
               ),
-            ),
-          ),
+            ),// Виджет для задания габаритов
+          ),// Скроллинг страницы
         ),
       ),
     );
