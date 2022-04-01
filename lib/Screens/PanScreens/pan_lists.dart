0cf5bd115132e716d104808fa9c55ad566c9ac52
tex1 <ivanagrebenkin@gmail.com>
2022-04-01 15:10:39 +0700

    В файле pan_lists.dart созданы списки для кастрюль.

--- lib/Screens/PanScreens/pan_lists.dart
+++ lib/Screens/PanScreens/pan_lists.dart
@@ -1,7 +1,15 @@
-List <String> listOfPanTypes = <String>['...', 'Кастрюля с ободком', 'Кастрюля без ободка',
-  'Позница', ];
+List <String> listOfPanTypes = <String>['...','Кастрюля с ободком','Кастрюля без ободка',];
 
+List <String> typesOfPanWithRimShapesList = <String>['...','Цилиндрическая','Сферическая',]; // Типы форм кастрюль с ободком
+List <String> typesOfPanWithoutRimShapesList = <String>['...','Цилиндрическая','Сферическая','Грушевидная','Позница',]; // Типы форм кастрюль без ободка
 
+List <String> panCylindricalWithRimSizeList = <String>['...','2,0 л','3,0 л','4,0 л','5,5 л',]; // Литраж кастрюль цилиндрических с ободком
+List <String> panSphericalWithRimSizeList = <String>['...','2,0 л','3,0 л','4,0 л','5,5 л',]; // Литраж кастрюль сферических с ободком
+
+List <String> panCylindricalWithoutRimSizeList = <String>['...','1,5 л','2,0 л','3,5 л','5,5 л','8,0 л','9,0 л','12,0 л','14,0 л',]; // Литраж кастрюль цилиндрических без ободка
+List <String> panSphericalWithoutRimSizeList = <String>['...','4,0 л','5,5 л','8,0 л',]; // Литраж кастрюль сферических без ободка
+List <String> panPearShapedWithoutRimSizeList = <String>['...','4,0 л','5,5 л','8,0 л',]; // Литраж кастрюль грушевидных без ободка
+List <String> poznicaSizeList = <String>['...','7,5 л (2 вставки)','9,0 л (3 вставки)','12,0 л (4 вставки)',]; // Литраж позниц
 
 
 ///////////// Список группы изделий ///////////////
